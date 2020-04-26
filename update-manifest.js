7a6d4c8de370e060a818298a46adce1377755d15
Valentin Semirulnik <v7rulnik@gmail.com>
2020-04-26 22:54:32 +0300

    enhance manifest dynamically

---
+++ update-manifest.js
@@ -0,0 +1,33 @@
+var fs = require("fs");
+
+const manifestPath = "./build/manifest.json";
+const manifest = require(manifestPath);
+const assetManifest = require("./build/asset-manifest.json");
+
+const cssEntrypoints = assetManifest.entrypoints.filter((file) =>
+  file.endsWith(".css")
+);
+const jsEntrypoint = assetManifest.entrypoints.filter((file) =>
+  file.endsWith(".js")
+);
+
+const contentScripsOtps = {
+  content_scripts: [
+    {
+      matches: ["http://gitlab.k8s.alfa.link/*/-/jobs/*/raw"],
+      css: cssEntrypoints,
+      js: jsEntrypoint,
+    },
+  ],
+};
+
+const extendedManifest = {
+  ...manifest,
+  ...contentScripsOtps,
+};
+
+console.log("New manifest will be:");
+const json = JSON.stringify(extendedManifest, null, "  ");
+console.log(json);
+
+fs.writeFileSync(manifestPath, json);
