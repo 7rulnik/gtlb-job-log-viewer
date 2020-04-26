96fea70d417eeb0739c174029043eab808e2ca9a
Valentin Semirulnik <v7rulnik@gmail.com>
2020-04-26 23:38:37 +0300

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const reactRoot = document.createElement('div')
 document.body.appendChild(reactRoot)
@@ -13,8 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   reactRoot
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
